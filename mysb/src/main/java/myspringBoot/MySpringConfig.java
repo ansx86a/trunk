549dd1f78a2e0289d0827111b993881f24876672
ansx86a <ansx86a@gmail.com>
2019-11-27 14:56:27 +0800

    可以有簡單的restful api

---
+++ mysb/src/main/java/myspringBoot/MySpringConfig.java
@@ -0,0 +1,9 @@
+package myspringBoot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan({"hello"})
+@Configuration
+public class MySpringConfig {
+}
