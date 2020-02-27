521ddde20862f9b62720692525c776bc79924bff
ansx86a <ansx86a@gmail.com>
2020-02-27 16:50:11 +0800

    加入swagger的測試

--- mysb/src/main/java/hello/rest/GreetingController.java
+++ mysb/src/main/java/hello/rest/GreetingController.java
@@ -1,5 +1,6 @@
 package hello.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class GreetingController {
      * @param name
      * @return
      */
-    @RequestMapping("/greeting")
+    @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
