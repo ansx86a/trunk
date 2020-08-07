13e2ecc64aba759de01a4a5839b0a0fd0c766ff1
ansx86a <ansx86a@gmail.com>
2020-08-07 14:42:36 +0800

    加入springboot上傳檔案的例子

---
+++ mysb/src/main/java/hello/rest/UploadController.java
@@ -0,0 +1,22 @@
+package hello.rest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+public class UploadController {
+    @PostMapping("/upload")
+    public String upload(MultipartFile file, String aaa, @RequestParam("bbb") String ccc) throws IOException {
+        System.out.println(new String(file.getBytes()));
+        System.out.println(aaa);
+        System.out.println(ccc);
+        return "abcd";
+    }
+
+}
